bf1b9003c11153df2259c1af0f5c84d3591c574b
Matias Gonzalvez <matiasdgonazlvez@gmail.com>
2024-07-03 01:20:13 -0300

    Criando página de Contato e Sobre.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav class="bg-[#ff9c06] p-4">
-      <div class="max-w-7xl mx-auto flex justify-between items-center">
-        <div class="flex items-center">
-          <span class="text-white text-lg font-semibold">Pousada Ypuã</span>
+    <nav className="bg-[#ff9c06] p-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div >
+          <a className="flex items-center" href={"/"}>
+          <img className="mr-5" src={"../logo-pousada-quinta-do-ypua.webp"} alt="logo" />
+          <span className="text-white text-lg font-semibold">Pousada Ypuã</span>
+          </a>
         </div>
 
-        <div class="space-x-4 text-white">
+        <div className="space-x-4 text-white">
           <Link to={"/"}>Home</Link>
-          <Link>Contato</Link>
-          <Link>Sobre</Link>
+          <Link to={"/contato"}>Contato</Link>
+          <Link to={"/sobre"}>Sobre</Link>
         </div>
       </div>
     </nav>
